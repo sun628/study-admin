43423b16ba7e7138654cf67aff604f3d11474fff
wangyang <15255096386@163.com>
2023-03-21 10:58:51 +0800

    新增eslint和prettier

--- src/views/type-script/02.任意类型.vue
+++ src/views/type-script/02.任意类型.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-row>
-    <el-col :span="12">
-      <doc title="Any 类型 和 unknown 顶级类型">
-        <p>1.没有强制限定哪种类型，随时切换类型都可以 我们可以对 any 进行任何操作，不需要检查类型</p>
-        <highlight :code="anyType"></highlight>
-      </doc>
-    </el-col>
-  </el-row>
+	<el-row>
+		<el-col :span="12">
+			<doc title="Any 类型 和 unknown 顶级类型">
+				<p>1.没有强制限定哪种类型，随时切换类型都可以 我们可以对 any 进行任何操作，不需要检查类型</p>
+				<highlight :code="anyType"></highlight>
+			</doc>
+		</el-col>
+	</el-row>
 </template>
 <script setup lang="ts">
 const anyType = `let anys:any = 123
