9a460eba03516cbdb660361c26ea6f7a3714feae
wangyang <15255096386@163.com>
2023-07-03 22:11:18 +0800

    useEcharts优化

--- src/hooks/useEcharts.ts
+++ src/hooks/useEcharts.ts
@@ -1,27 +1,53 @@
-import { onDeactivated, onBeforeUnmount } from 'vue';
+import { onMounted, onUnmounted, onDeactivated } from 'vue';
 import * as echarts from 'echarts';
+import { useDebounceFn } from '@vueuse/core';
 
 /**
  * @description 使用 Echarts (只是为了添加图表响应式)
- * @param {Element} myChart Echarts实例 (必传)
- * @param {Object} options 绘制Echarts的参数 (必传)
+ * @param {Element} chartRef Echarts实例 (必传)
+ * @returns {Object} chartInstance 实例
+ * @returns {function} setOption 设置图表配置项
+ * @example const { chartInstance, setOption } = useECharts(chartRef);
  * */
-export const useEcharts = (myChart: echarts.ECharts, options: echarts.EChartsCoreOption) => {
-	if (options && typeof options === 'object') {
-		myChart.setOption(options);
-	}
-	const echartsResize = () => {
-		myChart && myChart.resize();
+
+export default function useECharts(chartRef: Ref<HTMLElement>) {
+	let chartInstance: echarts.ECharts | null = null;
+	const initChart = () => {
+		const chart = echarts.getInstanceByDom(chartRef.value); // 获取已有实例
+		if (chart) {
+			chartInstance = chart;
+		} else {
+			chartInstance = echarts.init(chartRef.value); // 创建实例
+		}
+	};
+	const setOption = (options: echarts.EChartsOption): void => {
+		chartInstance?.setOption(options, true);
 	};
 
-	window.addEventListener('resize', echartsResize);
+	// 防抖
+	const resizeChart = useDebounceFn(() => {
+		chartInstance?.resize();
+	}, 500);
+
+	const onDestroy = () => {
+		window.removeEventListener('resize', resizeChart);
+		chartInstance?.dispose();
+		chartInstance = null; // 销毁实例
+	};
+
+	onMounted(() => {
+		initChart();
+		window.addEventListener('resize', resizeChart);
+	});
 
 	// 防止 echarts 页面 keepAlive 时，还在继续监听页面
 	onDeactivated(() => {
-		window.removeEventListener('resize', echartsResize);
+		onDestroy();
 	});
 
-	onBeforeUnmount(() => {
-		window.removeEventListener('resize', echartsResize);
+	onUnmounted(() => {
+		onDestroy();
 	});
-};
+
+	return { chartInstance, setOption };
+}
