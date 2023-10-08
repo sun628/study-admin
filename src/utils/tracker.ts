09d8530f0797da30b36ae03ce3abd25ac9f595de
wangyang <15255096386@163.com>
2023-10-08 10:30:53 +0800

    埋点

--- src/utils/tracker.ts
+++ src/utils/tracker.ts
@@ -3,7 +3,6 @@ import { useUserStore } from '@/store/modules/user';
 const userStore = useUserStore();
 // 自定义类型，合并 Tracker 类的构造函数参数与配置对象参数类型
 type TrackerOptions = ConstructorParameters<typeof Tracker>[0];
-console.log('userStore2', userStore.userInfo);
 
 const trackerOptions: TrackerOptions = {
 	uuid: userStore.userInfo.username,
