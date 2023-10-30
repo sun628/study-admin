d1403e5f8eee86b8c74e63323ddd73da4b70e096
wangyang <15255096386@163.com>
2023-10-30 16:13:55 +0800

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-import { App, defineAsyncComponent, AsyncComponentLoader } from 'vue';
-import SvgIcon from '@/components/svg-icon/index.vue';
+import { App, defineAsyncComponent, AsyncComponentLoader, Plugin } from 'vue';
+import SvgIcon from '@/components/svg-icon/SvgIcon.vue';
 import * as ElementPlusIcons from '@element-plus/icons-vue';
 
 // //引入依赖和语言
@@ -12,20 +12,42 @@ hljs.registerLanguage('xml', xml);
 import hljsVuePlugin from '@highlightjs/vue-plugin';
 // 获取所有组件，该方法返回一个对象
 
-export default function install(app: App) {
-	app.component('SvgIcon', SvgIcon);
+// export default function install(app: App) {
+// 	app.component('SvgIcon', SvgIcon);
 
-	app.use(hljsVuePlugin, {
-		languages: ['xml', 'typescript'],
-	});
-	// 遍历docs文件并注册异步组件
-	const components = import.meta.glob('./docs/*.vue');
-	for (const [key, value] of Object.entries(components)) {
-		const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
-		app.component(name, defineAsyncComponent(value as AsyncComponentLoader));
-	}
-	// 注册element Icons组件
-	Object.keys(ElementPlusIcons).forEach((key) => {
-		app.component(key, ElementPlusIcons[key as keyof typeof ElementPlusIcons]);
-	});
-}
+// 	app.use(hljsVuePlugin, {
+// 		languages: ['xml', 'typescript'],
+// 	});
+// 	// 遍历docs文件并注册异步组件
+// 	const components = import.meta.glob('./docs/*.vue');
+// 	for (const [key, value] of Object.entries(components)) {
+// 		const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
+// 		app.component(name, defineAsyncComponent(value as AsyncComponentLoader));
+// 	}
+// 	// 注册element Icons组件
+// 	Object.keys(ElementPlusIcons).forEach((key) => {
+// 		app.component(key, ElementPlusIcons[key as keyof typeof ElementPlusIcons]);
+// 	});
+// }
+
+const GlobalCom: Plugin = {
+	install(app: App) {
+		app.component('SvgIcon', SvgIcon);
+
+		app.use(hljsVuePlugin, {
+			languages: ['xml', 'typescript'],
+		});
+		// 遍历docs文件并注册异步组件
+		const components = import.meta.glob('./docs/*.vue');
+		for (const [key, value] of Object.entries(components)) {
+			const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
+			app.component(name, defineAsyncComponent(value as AsyncComponentLoader));
+		}
+		// 注册element Icons组件
+		Object.keys(ElementPlusIcons).forEach((key) => {
+			app.component(key, ElementPlusIcons[key as keyof typeof ElementPlusIcons]);
+		});
+	},
+};
+
+export default GlobalCom;
