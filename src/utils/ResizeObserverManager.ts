ff6807cd485d3afeab7b900a9b27fb766125bd74
暖阳 <15255096386@163.com>
2024-01-19 19:40:28 +0800

    update

--- src/utils/ResizeObserverManager.ts
+++ src/utils/ResizeObserverManager.ts
@@ -1,29 +1,60 @@
+/**
+ * @description ResizeObserverManager 类 (添加图表响应式)
+ */
 export class ResizeObserverManager {
-	observers: Map<Element, Set<() => void>>;
-	resizeObserver: ResizeObserver;
+	private observers: WeakMap<Element, Set<() => void>>;
+	private resizeObserver: ResizeObserver;
+
 	constructor() {
-		this.observers = new Map();
+		this.observers = new WeakMap();
 		this.resizeObserver = new ResizeObserver((entries) => {
 			entries.forEach((entry) => {
 				const resizeCallbacks = this.observers.get(entry.target);
-				resizeCallbacks?.forEach((callback) => callback());
+				resizeCallbacks?.forEach((callback) => {
+					try {
+						callback();
+					} catch (error) {
+						console.error('Resize observer callback error:', error);
+					}
+				});
 			});
 		});
 	}
 
+	/**
+	 * @description 添加观察者
+	 * @param {Element} element 目标元素
+	 * @param {() => void} callback 回调函数
+	 * @example resizeObserverManager.observe(chartRef.value, resizeChart);
+	 */
 	observe(element: Element, callback: () => void) {
-		if (!this.observers.has(element)) {
-			this.observers.set(element, new Set());
+		if (!(element instanceof Element)) {
+			throw new Error('The provided target is not an Element.');
+		}
+
+		let callbacks = this.observers.get(element);
+		if (!callbacks) {
+			callbacks = new Set();
+			this.observers.set(element, callbacks);
 			this.resizeObserver.observe(element);
 		}
-		const observerSet = this.observers.get(element);
-		observerSet && observerSet.add(callback);
+		// Avoid adding the same callback multiple times
+		if (!callbacks.has(callback)) {
+			callbacks.add(callback);
+		}
 	}
 
+	/**
+	 * @description 移除观察者
+	 * @param {Element} element 目标元素
+	 * @param {() => void} callback 回调函数
+	 * @example resizeObserverManager.unobserve(chartRef.value, resizeChart);
+	 */
 	unobserve(element: Element, callback: () => void) {
 		const callbacks = this.observers.get(element);
 		if (callbacks) {
 			callbacks.delete(callback);
+			// Unobserve the element if no callbacks are left
 			if (callbacks.size === 0) {
 				this.resizeObserver.unobserve(element);
 				this.observers.delete(element);
