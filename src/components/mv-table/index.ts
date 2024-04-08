6f27a0328cc2ec5e1af42f6ab1f29a788eca3711
暖阳 <15255096386@163.com>
2024-04-08 10:47:39 +0800

    types: add MvTableInstance type definition

--- src/components/mv-table/index.ts
+++ src/components/mv-table/index.ts
@@ -1,6 +1,9 @@
 import MvTable from './MvTable.vue';
+import type { ComponentExposed } from '@/hooks/useCompRef';
 
 export default MvTable;
+
+export type MvTableInstance = ComponentExposed<typeof MvTable>;
 
 export * from './type';
 
